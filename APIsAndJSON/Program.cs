e7ce228fb4b1f81fbb86254b8fd7a7659ebfbe46
bryantwhitsett <bryantwhitsett71@gmail.com>
2026-04-13 05:18:29 -0400

    Works like a charm or should I say like a chocolate factory

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace APIsAndJSON
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            HttpClient client = new HttpClient();
+            
+            for (int i = 0; i < 5; i++)
+            {
+                string ronResponse = await client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+                JArray ronArray = JArray.Parse(ronResponse);
+                string ronQuote = ronArray[0].ToString();
+                
+                string kanyeResponse = await client.GetStringAsync("https://api.kanye.rest");
+                JObject kanyeObject = JObject.Parse(kanyeResponse);
+                string kanyeQuote = kanyeObject["quote"].ToString();
+                
+                Console.WriteLine("Ron: " + ronQuote);
+                Console.WriteLine("Kanye: " + kanyeQuote);
+                Console.WriteLine();
+            }
         }
     }
 }
